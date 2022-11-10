979a522b63f85ba5e66848147c83d1f0122e4103
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-11-10 18:35:18 +0500

    Lecture 10

--- src/Home.js
+++ src/Home.js
@@ -2,45 +2,47 @@ import React, { useState } from 'react'
 
 const Home = () => {
 
-    // let name = 'zinnia'
-    const [name, setName] = useState('zinnia')
-    // console.log("name " + name + " & set name " + setName)
-    const [price, setPrice] = useState(200)
-
-    const handleClick = (e) => {
-        // name = 'cherry'
-        setName('cherry')
-        setPrice(300)
-        console.log("clicked " + name)
-        console.log("name " + name + " & set name " + setName)
-        // alert("clicked name ," + name)
-
-    }
-    const handleClickAgain = (name) => {
-        console.log(("hi my name is " + name))
-        alert("this is  " + name)
-    }
+    const [blogs, setBlogs] = useState([
+        {
+            id: 1,
+            title: 'My new Website',
+            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            author: 'lily'
+        },
+        {
+            id: 2,
+            title: 'React Study',
+            body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+            author: 'zinnia'
+        },
+        {
+            id: 3,
+            title: 'Learning JavaScript',
+            body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ",
+            author: 'Jenny'
+        }
+    ])
+    // console.log(blogs)
 
     return (
         <div className='home'>
-            <p>{name}</p>
-            <p>price of handfree is now: {price}</p>
-            <button
-                type="button"
-                className="btn btn-outline-success"
-                onClick={handleClick}
-            >
-                Click Me
-            </button>
-            <button
-                type="button"
-                className="btn btn-outline-primary"
-                // onClick={handleClickAgain('lily')}
-                // onClick={() => { console.log('check it') }}
-                onClick={() => handleClickAgain('lily')}
-            >
-                Click Me Again
-            </button>
+            {
+                blogs.map(
+                    (blog) => (
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>
+                                {blog.title}
+                            </h2>
+                            <p>
+                                Written by: {blog.author}
+                            </p>
+                            <p>
+                                {blog.body}
+                            </p>
+                        </div>
+                    )
+                )
+            }
         </div >
     )
 }
