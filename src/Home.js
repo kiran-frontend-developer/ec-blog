45a5cffc0c366d4c06f3eb067cd91760421516e0
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-11-18 00:41:57 +0500

    fetch() then get response, then separate data from response

--- src/Home.js
+++ src/Home.js
@@ -4,26 +4,27 @@ import BlogList from "./BlogList";
 const Home = () => {
   //hooks start
   const [blogs, setBlogs] = useState(
-    [
-    {
-      id: 1,
-      title: "My new Website",
-      body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      author: "lily",
-    },
-    {
-      id: 2,
-      title: "React Study",
-      body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
-      author: "zinnia",
-    },
-    {
-      id: 3,
-      title: "Learning JavaScript",
-      body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ",
-      author: "Jenny",
-    },
-  ]
+    // [
+    //   {
+    //     id: 1,
+    //     title: "My new Website",
+    //     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    //     author: "lily",
+    //   },
+    //   {
+    //     id: 2,
+    //     title: "React Study",
+    //     body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',",
+    //     author: "zinnia",
+    //   },
+    //   {
+    //     id: 3,
+    //     title: "Learning JavaScript",
+    //     body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, ",
+    //     author: "Jenny",
+    //   },
+    // ]
+    null
   );
   //   console.log(blogs)
 
@@ -33,20 +34,36 @@ const Home = () => {
   // Increament a number on click and then change will also dispaly vis useEffect
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.log("use effect run");
-    console.log(blogs);
-    console.log(name);
-  }, [name]);
+  useEffect(
+    () => {
+      // console.log("use effect run");
+      // console.log(blogs);
+      // console.log(name);
+      fetch("http://localhost:8000/blogs") // this full line returning a promise
+        .then((res) => {
+          // console.log(res);
+          let response = res.json();
+          console.log(response);
+          return response;
+        })
+        .then((data) => {
+          console.log(data);
+          console.log(data[1]);  // array start from 0 index its mean 1 index having 2nd row
+        });
+      // .catch(err => console.log(err))
+      // .finally(rslt => console.log(rslt))
+    },
+    // [name]
+    []
+  );
 
-  useEffect(() => {
-    console.log(count)
-  }, []
-  )
+  // useEffect(() => {
+  //   console.log(count);
+  // }, []);
 
-  useEffect(() => {
-    console.log('count: ' + count + ' , setCount: ' + setCount)
-  }, [count])
+  // useEffect(() => {
+  //   console.log("count: " + count + " , setCount: " + setCount);
+  // }, [count]);
   //hooks end
 
   //methods start
@@ -61,9 +78,9 @@ const Home = () => {
 
   //increment methhod
   const incrementNum = () => {
-    setCount(count + 1)
+    setCount(count + 1);
     console.log(setCount);
-    console.log(count)
+    console.log(count);
   };
 
   //methods end
@@ -77,7 +94,7 @@ const Home = () => {
           <p>{blog.body}</p>
         </div>
       ))} */}
-      <BlogList
+      {/* <BlogList
         blogs_send={blogs}
         title="Blogs List"
         handleDelete={deleteBlog}
@@ -86,21 +103,27 @@ const Home = () => {
         blogs_send={blogs.filter((blog) => blog.author === "zinnia")}
         title="Zinnia's Blogs"
         handleDelete={deleteBlog}
-      />
+      /> */}
 
       {/* button for useEffect dependency */}
-      <button
+      {/* <button
         onClick={() => setName("Name changed")}
         className="btn btn-outline-danger d-grid gap-2 col-6 mx-auto my-5"
         type="button"
       >
         useEffect Dependency
       </button>
-      <p>{name}</p>
+      <p>{name}</p> */}
 
       {/* increment button*/}
       <p>{count}</p>
-      <button type="button" className="btn btn-danger me-2 p-1 text-decoration-none" onClick={() => incrementNum()}>increment button</button>
+      {/* <button
+        type="button"
+        className="btn btn-danger me-2 p-1 text-decoration-none"
+        onClick={() => incrementNum()}
+      >
+        increment button
+      </button> */}
 
       {/*  arithmetic operators 
                    + - * / %
