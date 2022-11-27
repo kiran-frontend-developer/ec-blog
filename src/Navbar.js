a6a62f0f44076bb8702b87d99009bf83d95d0c15
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-11-27 15:48:36 +0500

    Study of routers links and implement in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <nav className="navbar border border-primary">
             <h1 className='text-danger'>The Ecounter Blog</h1>
             <div className="links">
-                <button type="button" className="btn btn-danger me-2 p-1 text-decoration-none" href="/" >Home</button>
-                <button type="button" className="btn btn-danger me-2 p-1 text-decoration-none" href="/create" >New Blog</button>
+                <Link type="button" className="btn btn-danger me-2 p-1 text-decoration-none" to="/" >Home</Link>
+                <Link type="button" className="btn btn-danger me-2 p-1 text-decoration-none" to="/create" >New Blog</Link>
 
                 {/* <a href="/" className='me-2 p-1 text-decoration-none border border-secondary '>Home</a>
                 <a href="/create" className=' me-2 p-1 text-decoration-none border border-danger'>New Blog</a> */}
