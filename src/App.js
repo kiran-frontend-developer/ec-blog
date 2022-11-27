cab08b9b8f5f8cb64007732a05405a8c401f8f92
kiran-frontend-developer <117917616+kiran-frontend-developer@users.noreply.github.com>
2022-11-27 15:01:43 +0500

    install react-router-dom@5 , establish, use it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App text-center m-0 border border-danger">
       <Navbar />
       <div className="content border border-secondary p-5">
-        <Home />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
   );
