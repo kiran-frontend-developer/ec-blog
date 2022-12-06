695e5bbd134c107e60c747ef0175cdb972cdc694
kiran-frontend-developer <kiran.hanif.dev@gmail.com>
2022-12-06 16:35:23 +0500

    add useParams hook to grab the right id with will assiciate with route as segment

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
+  // to grabe the right blog id we use hook useParams
+  const {} = useParams();
+
   return (
     <div className="blog-details">
       <h2>Blog Details</h2>
