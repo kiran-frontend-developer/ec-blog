0ba34c86025080d78d94dae7b1270fc36303d14c
kiran-frontend-developer <kiran.hanif.dev@gmail.com>
2022-12-06 20:03:31 +0500

    destructure the useParams ,to get id of required route parameter

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,11 +3,11 @@ import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
   // to grabe the right blog id we use hook useParams
-  const {} = useParams();
+  const { id } = useParams(); //destructure it, for required route parameter, use id there
 
   return (
     <div className="blog-details">
-      <h2>Blog Details</h2>
+      <h2>Blog Details {id}</h2>
     </div>
   );
 };
