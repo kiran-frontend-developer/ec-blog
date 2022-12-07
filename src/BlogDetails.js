fff98199b8315afb6cc0559a9509cc4a199ff70f
kiran-frontend-developer <kiran.hanif.dev@gmail.com>
2022-12-07 12:52:45 +0500

    useFetch hook is used to show data on details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   // to grabe the right blog id we use hook useParams
   const { id } = useParams(); //destructure it, for required route parameter, use id there
+
+  // to show data of the selected blog here , useFetch is used here
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  console.log(blog);
 
   return (
     <div className="blog-details">
