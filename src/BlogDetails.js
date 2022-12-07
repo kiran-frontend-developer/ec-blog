c551fe1cebbcf22ea41c9a67034b2213cad34ad4
kiran-frontend-developer <kiran.hanif.dev@gmail.com>
2022-12-07 17:54:53 +0500

    to show blog apply isPending and error before and check is blog empty, then apply content in article

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -16,7 +16,22 @@ const BlogDetails = () => {
 
   return (
     <div className="blog-details">
-      <h2>Blog Details {id}</h2>
+      {/* <h2>Blog Details {id}</h2> */}
+
+      {/* display single blog detail  */}
+      {/* for this purpose we 1st check is data loading or loaded , then error check apply */}
+      {isPending && <div>Loading....</div>}
+      {error && <div>{error}</div>}
+
+      {/* here blog details need to how . in useEffect data is null by defualt so apply check there data show only for non-null values*/}
+      {blog && (
+        // here return some kind of template
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Wirttern By {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
